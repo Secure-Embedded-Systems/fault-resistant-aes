2002f4bced88dd9a6e064e9b1328ba2337bbb954
Conor <conorpp@vt.edu>
2016-03-27 22:36:26 -0400

    reverse transpose

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 
 
 // transpose a block
+// TODO do it in place
 void bs_transpose(WORD * transpose, WORD * blocks)
 {
     int i,j,k;
@@ -18,6 +19,29 @@ void bs_transpose(WORD * transpose, WORD * blocks)
             {
                 // TODO make const time
                 transpose[i*WORD_SIZE + j] |= (w & (ONE << j)) ? (ONE<<k) : 0;
+            }
+        }
+    }
+}
+
+void bs_transpose_rev(WORD * transpose, WORD * blocks)
+{
+    int i,j,k;
+    WORD w;
+    for(k=0; k < BLOCK_SIZE; k++)
+    {
+        w = blocks[k];
+        for(j=0; j < WORD_SIZE; j++)
+        {
+            uint64_t bit = (w & (ONE << j)) ? (ONE << (k % WORD_SIZE)) : 0;
+            // TODO make const time
+            if ( k >= WORD_SIZE )
+            {
+                transpose[j * 2 + 1] |= bit;
+            }
+            else
+            {
+                transpose[j * 2] |= bit;
             }
         }
     }
@@ -44,10 +68,20 @@ extern uint8_t INPUT[WORD_SIZE/8][BLOCK_SIZE/8 + 1];
 int main()
 {
     WORD blocks[ BLOCK_SIZE ];
+    WORD blocks_tmp[ BLOCK_SIZE ];
     memset(blocks,0, sizeof(blocks));
-
+    memset(blocks_tmp,0, sizeof(blocks));
     bs_transpose(blocks,(WORD*)INPUT);
+    bs_transpose_rev(blocks_tmp,blocks);
+    
+    printf("original:\n");
+    bs_dump((WORD*)INPUT);
+
+    printf("transpose:\n");
     bs_dump(blocks);
+
+    printf("double transpose:\n");
+    bs_dump(blocks_tmp);
 
 
     return 0;
