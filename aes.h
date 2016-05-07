491bc39611d250e5cb1c0782991d20415f282b77
Conor Patrick <conorpp@vt.edu>
2016-05-07 16:57:07 -0400

    started ctr

--- aes.h
+++ aes.h
@@ -6,4 +6,7 @@
 void aes_ecb_encrypt(uint8_t * outputb, uint8_t * inputb, size_t size, uint8_t * key);
 void aes_ecb_decrypt(uint8_t * outputb, uint8_t * inputb, size_t size, uint8_t * key);
 
+void aes_ctr_encrypt(uint8_t * outputb, uint8_t * inputb, size_t size, uint8_t * key, uint8_t * iv);
+void aes_ctr_decrypt(uint8_t * outputb, uint8_t * inputb, size_t size, uint8_t * key, uint8_t * iv);
+
 #endif
--- main.c
+++ main.c
@@ -30,22 +30,12 @@ void dump_block(word_t * h, int len)
     printf("\n");
 }
 
-
-// TODO AES CTR
-int main(int argc, char * argv[])
+void aes_ecb_test()
 {
-    if (argc < 2)
-    {
-        fprintf(stderr,"usage: %s <input-file>\n", argv[0]);
-        exit(1);
-    }
     uint8_t key_vector[16] = "\x2b\x7e\x15\x16\x28\xae\xd2\xa6\xab\xf7\x15\x88\x09\xcf\x4f\x3c";
     uint8_t pt_vector[16] =  "\x6b\xc1\xbe\xe2\x2e\x40\x9f\x96\xe9\x3d\x7e\x11\x73\x93\x17\x2a";
     uint8_t output[16];
     uint8_t input[16];
-
-    uint8_t key_s[KEY_SCHEDULE_SIZE];
-    memmove(input, pt_vector, sizeof(pt_vector));
 
     aes_ecb_encrypt(output, pt_vector,16,key_vector);
 
@@ -57,7 +47,28 @@ int main(int argc, char * argv[])
 
     printf("plain text: \n");
     dump_hex((uint8_t * )input,16);
+}
 
+
+// TODO AES CTR
+int main(int argc, char * argv[])
+{
+    if (argc < 2)
+    {
+        fprintf(stderr,"usage: %s <input-file>\n", argv[0]);
+        exit(1);
+    }
+    uint8_t key_vector[16] = "\x2b\x7e\x15\x16\x28\xae\xd2\xa6\xab\xf7\x15\x88\x09\xcf\x4f\x3c";
+    uint8_t iv_vector[16]  = "\xf0\xf1\xf2\xf3\xf4\xf5\xf6\xf7\xf8\xf9\xfa\xfb\xfc\xfd\xfe\xff";
+    uint8_t pt_vector[16] =  "\x6b\xc1\xbe\xe2\x2e\x40\x9f\x96\xe9\x3d\x7e\x11\x73\x93\x17\x2a";
+    
+    uint8_t output[16];
+    uint8_t input[16];
+
+    aes_ctr_encrypt(output,pt_vector,16,key_vector, iv_vector);
+
+    printf("cipher text: \n");
+    dump_hex(output,16);
     return 0;
 }
 
